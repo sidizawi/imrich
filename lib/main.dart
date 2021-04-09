4ac6a803f7de4ed10603322cb4a501116f152b96
Sidi Zawi <sidi@Sidis-MacBook-Air.local>
2021-04-10 00:37:00 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+	MaterialApp(
+    debugShowCheckedModeBanner: false,
+		home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+			appBar: AppBar(
+				title: Text("I AmRich"),
+        backgroundColor: Colors.blueGrey[900],
+			),
+      body: Center(
+        child: Image(
+          image: AssetImage("images/diamond.png")
+        ),
+      ),
+		),
+	),
+);
